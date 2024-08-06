242cd39bea60e2915fe1ed21607e1ffe8ec36492
andresdlRoca <andresdelaroca11@gmail.com>
2024-08-06 16:01:32 -0600

    Created basic routes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
