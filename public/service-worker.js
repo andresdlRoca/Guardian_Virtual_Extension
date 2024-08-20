a6bf7de0108e52a3f6f6a220b7f27ab799344f7b
andresdlRoca <andresdelaroca11@gmail.com>
2024-08-20 12:12:49 -0600

    Integracion con API de deteccion basada en listas

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,5 @@
+const API_URL = 'http://localhost:8000'
+
 chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
       id: "analyze_phish",
@@ -13,11 +15,95 @@ chrome.runtime.onInstalled.addListener(() => {
         function: grabAndParseSelectedTextWithLinks
       }, (results) => {
         if (results && results[0]) {
-          console.log(results[0].result);  // Handle the parsed text with links here.
+          phishingAnalysis(results[0].result);
         }
       });
     }
   });
+
+  async function phishingAnalysis(text) {
+    try {
+      // Should have text and urls separated
+      const urls = text.match(/\bhttps?:\/\/\S+/gi);
+      if (!urls) {
+        console.log('No URLs found in the selected text.');
+        return;
+      }
+
+      const fqdn = urls.map(url => new URL(url).hostname);
+
+      console.log('URLs:', urls);
+      console.log('FQDNs:', fqdn);
+
+      // Check each URL against the whitelist
+      const whitelistResults = await Promise.all(fqdn.map(checkWhitelist));
+      console.log('Whitelist results:', whitelistResults);
+
+      // Check each URL against the blacklist
+      const blacklistResults = await Promise.all(fqdn.map(checkBlacklist));
+      console.log('Blacklist results:', blacklistResults);
+
+      // Check each URL against the popularity rank
+      const popularityResults = await Promise.all(fqdn.map(checkPopular));
+      console.log('Popularity rank results:', popularityResults);
+      
+      // Have a variable to store text without URLs
+      const textWithoutUrls = text.replace(/\bhttps?:\/\/\S+/gi, '');
+      console.log('Text without URLs:', textWithoutUrls);
+
+    } catch (error) {
+      console.error('Error while analyzing phishing:', error);
+    }
+  }
+
+  async function fetchData(apiEndpoint, arg) {
+    try {
+      const response = await fetch(`${apiEndpoint}?url=${encodeURIComponent(arg)}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    if (!response.ok) {
+        throw new Error(`Error while fetching response from API: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+    } catch (error) {
+      console.error('Error while fetching data:', error);
+      return { error: error.message };
+    }
+  }
+
+  async function checkWhitelist(url) {
+    try {
+      const apiEndpoint = `${API_URL}/whitelist`;
+      const result = await fetchData(apiEndpoint, url);
+      return result;
+    } catch (error) {
+      console.error('Error while checking whitelist:', error);
+    }
+  }
+
+  async function checkBlacklist(url) {
+    try {
+      const apiEndpoint = `${API_URL}/blacklist`;
+      const result = await fetchData(apiEndpoint, url);
+      return result;
+    } catch (error) {
+      console.error('Error while checking blacklist:', error);
+    }
+  }
+
+  async function checkPopular(url) {
+    try {
+      const apiEndpoint = `${API_URL}/popularityrank`;
+      const result = await fetchData(apiEndpoint, url);
+      return result;
+    } catch (error) {
+      console.error('Error while checking popularity rank:', error);
+    }
+  }
   
   function grabAndParseSelectedTextWithLinks() {
     let selection = window.getSelection();
